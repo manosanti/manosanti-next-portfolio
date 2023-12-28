fd8d18be4348138212387ce88915081ea875962b
Lucas Santiago <luccassantiago.contato@gmail.com>
2023-12-28 17:57:17 -0300

    home-page
    added some infos and adjust

--- frontend/src/app/components/Sidebar.tsx
+++ frontend/src/app/components/Sidebar.tsx
@@ -9,6 +9,10 @@ import { MdArrowCircleLeft } from "react-icons/md";
 import { FaHouse } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
 import { ImLinkedin2 } from "react-icons/im";
+import { MdEmail } from "react-icons/md";
+import { IoBookOutline } from "react-icons/io5";
+import { FaFolderOpen } from "react-icons/fa6";
+import { MdLaptopChromebook } from "react-icons/md";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
@@ -16,7 +20,7 @@ const Sidebar = () => {
   return (
     <div className="flex">
       <div
-        className={`bg-neutral-950 h-screen p-5 pt-8 border-r-2 border-indigo-500	 ${
+        className={`bg-slate-950 h-screen p-5 pt-8 border-r-2 border-slate-800	 ${
           open ? "w-72" : "w-20"
         } relative duration-300`}
       >
@@ -27,15 +31,60 @@ const Sidebar = () => {
           onClick={() => setOpen(!open)}
         />{" "}
         <div className="mb-4 inline-flex items-center">
-          <Link href="/">
-          </Link>
-          <Link
-            href="/"
-            className={`inline-flex items-center`}
-          >
+          <Link href="/" className={`px-2 inline-flex items-center`}>
             <FaHouse className="mr-3" />
-            <span className={`inline-flex items-center ${!open && "hidden"}`}>Home</span>
+            <span className={`inline-flex items-center ${!open && "hidden"}`}>
+              Home
+            </span>
           </Link>
+        </div>
+        <div>
+          <p
+            className={`text-xs text-slate-400 font-semibold ${
+              !open && "scale-0"
+            }`}
+          >
+            Eu
+          </p>
+
+          <ul className="px-2 duration-300 rounded hover:bg-indigo-950 cursor-pointer">
+            <li className="inline-flex items-center mt-2">
+              <Link href="/" className={`inline-flex items-center`}>
+                <IoBookOutline className="mr-3" />
+                <span
+                  className={`inline-flex items-center ${!open && "hidden"}`}
+                >
+                  Sobre Mim
+                </span>
+              </Link>
+            </li>
+          </ul>
+
+          <ul className="px-2 duration-300 rounded hover:bg-indigo-950 cursor-pointer">
+            <li className="inline-flex items-center mt-2">
+              <Link href="/" className={`inline-flex items-center`}>
+                <FaFolderOpen className="mr-3" />
+                <span
+                  className={`inline-flex items-center ${!open && "hidden"}`}
+                >
+                  Projetos
+                </span>
+              </Link>
+            </li>
+          </ul>
+
+          <ul className="px-2 duration-300 rounded hover:bg-indigo-950 cursor-pointer">
+            <li className="inline-flex items-center mt-2">
+              <Link href="/" className={`inline-flex items-center`}>
+                <MdLaptopChromebook className="mr-3" />
+                <span
+                  className={`inline-flex items-center ${!open && "hidden"}`}
+                >
+                  Tecnologias e Ferramentas
+                </span>
+              </Link>
+            </li>
+          </ul>
         </div>
         <div>
           <p
@@ -45,30 +94,40 @@ const Sidebar = () => {
           >
             Socials
           </p>
-          <ul>
+          <ul className="px-2 duration-300 rounded hover:bg-indigo-950 cursor-pointer">
             <li className="inline-flex items-center mt-2">
-              <Link href="/">
-              <FaGithub className="mr-3" />
-                </Link> 
-              <Link
-                href="/"
-                className={`inline-flex items-center ${!open && "scale-0"}`}
-              >
-                {open && "Home"}
+              <Link href="/" className={`inline-flex items-center`}>
+                <FaGithub className="mr-3" />
+                <span
+                  className={`inline-flex items-center ${!open && "hidden"}`}
+                >
+                  GitHub
+                </span>
               </Link>
             </li>
           </ul>
 
-          <ul>
-          <li className="inline-flex items-center mt-2">
-              <Link href="/">
-              <ImLinkedin2 className="mr-3" />
-                </Link> 
-              <Link
-                href="/"
-                className={`inline-flex items-center ${!open && "scale-0"}`}
-              >
-                {open && "LinkedIn"}
+          <ul className="px-2 duration-300 rounded hover:bg-indigo-950 cursor-pointer">
+            <li className="inline-flex items-center mt-2">
+              <Link href="/" className={`inline-flex items-center`}>
+                <ImLinkedin2 className="mr-3" />
+                <span
+                  className={`inline-flex items-center ${!open && "hidden"}`}
+                >
+                  LinkedIn
+                </span>
+              </Link>
+            </li>
+          </ul>
+          <ul className="px-2 duration-300 rounded hover:bg-indigo-950 cursor-pointer">
+            <li className="inline-flex items-center mt-2">
+              <Link href="/" className={`inline-flex items-center`}>
+                <MdEmail className="mr-3" />
+                <span
+                  className={`inline-flex items-center ${!open && "hidden"}`}
+                >
+                  E-mail
+                </span>
               </Link>
             </li>
           </ul>
--- frontend/src/app/pages/Home.tsx
+++ frontend/src/app/pages/Home.tsx
@@ -3,9 +3,22 @@ import Sidebar from "../components/Sidebar";
 import Link from "next/link";
 import profilePhoto from "@/app/imgs/teste.png";
 import Img from "next/image";
-import easyAluga from '@/app/imgs/projects/easy-aluga.webp';
+import easyAluga from "@/app/imgs/projects/easy-aluga.webp";
 import { MdArrowOutward } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import uninoveLogo from "@/app/imgs/uninove-logo.png";
+import actmobLogo from "@/app/imgs/actmob-logo.jpeg";
+import proaLogo from "@/app/imgs/download.png";
+import oracleLogo from "@/app/imgs/downloadOracle.png";
+import { FiDownload } from "react-icons/fi";
+
+function calcMonthsActmobFromStart(startDate: Date): number {
+  const today = new Date();
+  const years = today.getFullYear() - startDate.getFullYear();
+  const months = today.getMonth() - startDate.getMonth();
+  return years * 12 + months;
+}
 
 const Home = () => {
   return (
@@ -19,86 +32,191 @@ const Home = () => {
             className="aspect-square rounded-lg overflow-hidden w-24 h-24"
           />
           <div className="ml-4">
-            <h1>Lucas Santiago</h1>
-            <span>
-              Front-end developer @{" "}
+            <h1 className="text-3xl font-bold">Lucas Santiago</h1>
+            <span className="text-slate-400">
+              Desenvolvedor Front-end @{" "}
               <Link href="/" className="text-blue-600">
                 Actmob
               </Link>
             </span>
-            <ul className="flex">
-              <li className="bg-indigo-700 mr-1 py-0 px-1 rounded">
-                <p>React</p>
+            <ul className="flex my-1">
+              <li className="text-xs bg-indigo-950 mr-1 py-0 px-2 rounded">
+                <p>ReactJS</p>
               </li>
-              <li className="bg-indigo-700 mr-1 py-0 px-1 rounded">
-                <p>React</p>
+              <li className="text-xs bg-indigo-950 mr-1 py-0 px-2 rounded">
+                <p>NextJS</p>
               </li>
-              <li className="bg-indigo-700 mr-1 py-0 px-1 rounded">
-                <p>React</p>
+              <li className="text-xs bg-indigo-950 mr-1 py-0 px-2 rounded">
+                <p>NodeJS</p>
               </li>
-              <li className="bg-indigo-700 mr-1 py-0 px-1 rounded">
-                <p>React</p>
+              <li className="text-xs bg-indigo-950 mr-1 py-0 px-2 rounded">
+                <p>TypeScript</p>
               </li>
-              <li className="bg-indigo-700 mr-1 py-0 px-1 rounded">
-                <p>React</p>
+              <li className="text-xs bg-indigo-950 mr-1 py-0 px-2 rounded">
+                <p>JavaScript</p>
               </li>
             </ul>
           </div>
         </div>
-        <div className="rounded border border-collapse border-slate-800 my-4 p-2">
-          <h3>📋 Sobre mim:</h3>
-          <p>
-            Hello, my name is Erick Nathan, I am a dedicated front-end
-            developer, with extensive experience in technologies such as NodeJS
-            and ReactJS, and a track record with notable clients such as Uber
-            Chip, Correios Celular and Carrefour, specializing in robust and
-            user-friendly solutions to the user, prioritizing scalability,
-            accessibility and optimized performance.
-          </p>
-          {/* <Link href={"/about"} className="text-right">Teste</Link> */}
-        </div>
-        {/* Card Experiences */}
-        <div className="grid grid-cols-2 gap-4">
-          <div className="rounded border border-collapse border-slate-800 my-4 p-2">
-            <h3>💼 Experiences</h3>
-            <p>
-              Hello, my name is Erick Nathan, I am a dedicated front-end
-              developer, with extensive experience in technologies such as
-              NodeJS and ReactJS, and a track record with notable clients such
-              as Uber Chip, Correios Celular and Carrefour, specializing in
-              robust and user-friendly solutions to the user, prioritizing
-              scalability, accessibility and optimized performance.
-            </p>
-            {/* <Link href={"/about"} className="text-right">Teste</Link> */}
+
+        {/* Card About Me */}
+        <div className="rounded-xl border border-collapse border-slate-800 my-4 p-7">
+          <div className="flex justify-between mb-4">
+            <h3 className="text-xl font-semibold ">📋 Sobre mim:</h3>
+            <Link href="/" className="text-xs flex items-center text-blue-600">
+              Saber Mais <IoIosArrowForward />
+            </Link>
           </div>
-          <div className="rounded border border-collapse border-slate-800 my-4 p-2">
-            <h3>💼 Experiences</h3>
-            <p>
-              Hello, my name is Erick Nathan, I am a dedicated front-end
-              developer, with extensive experience in technologies such as
-              NodeJS and ReactJS, and a track record with notable clients such
-              as Uber Chip, Correios Celular and Carrefour, specializing in
-              robust and user-friendly solutions to the user, prioritizing
-              scalability, accessibility and optimized performance.
-            </p>
+          <p className="text-slate-400">
+            Olá, meu nome é <strong>Lucas Santiago</strong>, sou um{" "}
+            <strong>Desenvolvedor Front-End </strong>
+            dedicado, com ampla experiência em tecnologias como{" "}
+            <strong>NodeJS</strong> e<strong> ReactJS</strong>. Atualmente, faço
+            parte da equipe da <strong>Actmob Marketing Digital</strong>, onde
+            trabalho para criar soluções robustas e amigáveis ao usuário. Busco
+            sempre priorizar a escalabilidade, acessibilidade e desempenho
+            otimizado em meus projetos.
+          </p>
+        </div>
+
+        {/* Card - Experiences */}
+        <div className="grid grid-cols-2 gap-6">
+          <div className="rounded-xl border border-collapse border-slate-800 my-4 p-7">
+            <div className="flex justify-between mb-4">
+              <h3 className="text-xl font-semibold ">💼 Experiências</h3>
+              <Link
+                href="/"
+                className="text-xs flex items-center text-blue-600"
+              >
+                Saber Mais <IoIosArrowForward />
+              </Link>
+            </div>
+            <div className="flex items-center divide-slate-700">
+              <Img
+                src={actmobLogo}
+                alt="Actmob Logo"
+                className="w-12 h-12 rounded-full p-1 border-2 border-slate-700 "
+              />
+              <div className="ml-3">
+                <h3 className="text-sm font-semibold">Actmob</h3>
+                <p className="text-slate-400 text-xs">
+                  Desenvolvedor Front-End
+                </p>
+                <p className="text-slate-500 text-xs">
+                  2023 - Atualmente •{" "}
+                  {Number(calcMonthsActmobFromStart(new Date(2023, 5, 19)))}{" "}
+                  meses
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center divide-slate-700 mt-3">
+              <Img
+                src={uninoveLogo}
+                alt="Uninove"
+                className="w-12 h-12 rounded-full p-1 border-2 border-slate-700 "
+              />
+              <div className="ml-3">
+                <h3 className="text-sm font-semibold">Uninove</h3>
+                <p className="text-slate-400 text-xs">
+                  Estudante em Análise e Desenvolvimento de Sistemas
+                </p>
+                <p className="text-slate-500 text-xs">2023 - 2025</p>
+              </div>
+            </div>
+
+            <div className="flex items-center divide-slate-700 mt-3">
+              <Img
+                src={oracleLogo}
+                alt="Oracle One Next Education Logo"
+                className="w-12 h-12 rounded-full p-1 border-2 border-slate-700 "
+              />
+              <div className="ml-3">
+                <h3 className="text-sm font-semibold">
+                  Oracle Next Education (Alura)
+                </h3>
+                <p className="text-slate-400 text-xs">
+                  Estudante em Desenvolvimento Front-End
+                </p>
+                <p className="text-slate-500 text-xs">
+                  fev. 2022 - jul. 2022 • 6 meses
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-center divide-slate-700 mt-3">
+              <Img
+                src={proaLogo}
+                alt="Proa Logo"
+                className="w-12 h-12 rounded-full p-1 border-2 border-slate-700 "
+              />
+              <div className="ml-3">
+                <h3 className="text-sm font-semibold">Instituto Proa</h3>
+                <p className="text-slate-400 text-xs">
+                  Estudante em Desenvolvimento Web Full-Stack
+                </p>
+                <p className="text-slate-500 text-xs">
+                  fev. 2022 - jul. 2022 • 6 meses
+                </p>
+              </div>
+            </div>
+
+            <div className="text-center mt-7">
+              <Link href="/">
+                <button className="bg-blue-600 p-2 w-full rounded-md flex items-center justify-center text-sm font-medium">
+                  Baixar meu currículo <FiDownload className="ml-2" />
+                </button>
+              </Link>
+            </div>
+
           </div>
 
+          <div className="rounded-xl border border-collapse border-slate-800 my-4 p-7">
+            <div className="flex justify-between mb-4">
+              <h3 className="text-xl font-semibold ">💼 Experiences</h3>
+              <Link
+                href="/"
+                className="text-xs flex items-center text-blue-600"
+              >
+                Saber Mais <IoIosArrowForward />
+              </Link>
+            </div>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              commodi dignissimos id est itaque ducimus a obcaecati qui earum
+              accusamus similique, dolore, possimus necessitatibus fugit error
+              consequatur veniam aspernatur aut?
+            </p>
+          </div>
         </div>
-          {/* My Projects */}
-          <div className="rounded border border-collapse border-slate-800 my-4 p-2">
-            <h3>📂 My projects</h3>
-            <div className="grid grid-cols-2">
-              <Img src={easyAluga} alt="Lucas Santiago" className="my-2" />
-              <div className="pl-4">
-                <h2 className="text-right">Easy Aluga</h2>
-                <p className="text-right">A white-label application to rent any vehicle, developed during the BRQ Easy Carros Challenge on FIAP.</p>
-                <div className="float-right grid grid-cols-2 gap-2">
-                  <button className="bg-blue-600 rounded hover:bg-blue-800 duration-300"><Link href="/" className="text-sm inline-flex items-center"><MdArrowOutward className="mr-2" />Visitar</Link></button>
-                  <button className="bg-transparent rounded border border-slate-700 hover:bg-slate-800 duration-300 py-1 px-3"><Link href="/" className="text-sm inline-flex items-center"><FaGithub className="mr-2" />Código-fonte</Link></button>
-                </div>
+        {/* My Projects */}
+        <div className="rounded border border-collapse border-slate-800 my-4 p-2">
+          <h3>📂 My projects</h3>
+          <div className="grid grid-cols-2">
+            <Img src={easyAluga} alt="Lucas Santiago" className="my-2" />
+            <div className="pl-4">
+              <h2 className="text-right">Easy Aluga</h2>
+              <p className="text-right">
+                A white-label application to rent any vehicle, developed during
+                the BRQ Easy Carros Challenge on FIAP.
+              </p>
+              <div className="float-right grid grid-cols-2 gap-2">
+                <button className="bg-blue-600 rounded hover:bg-blue-800 duration-300">
+                  <Link href="/" className="text-sm inline-flex items-center">
+                    <MdArrowOutward className="mr-2" />
+                    Visitar
+                  </Link>
+                </button>
+                <button className="bg-transparent rounded border border-slate-700 hover:bg-slate-800 duration-300 py-1 px-3">
+                  <Link href="/" className="text-sm inline-flex items-center">
+                    <FaGithub className="mr-2" />
+                    Código-fonte
+                  </Link>
+                </button>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
